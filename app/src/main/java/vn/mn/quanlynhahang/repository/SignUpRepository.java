d493a968f38a5b8d297cce8f49323c228d56f728
MN <57828955+MNgaminhhh@users.noreply.github.com>
2024-04-18 23:26:45 +0700

    create basic homeview with get data in firebase cloudstore

--- app/src/main/java/vn/mn/quanlynhahang/repository/SignUpRepository.java
+++ app/src/main/java/vn/mn/quanlynhahang/repository/SignUpRepository.java
@@ -1,17 +1,29 @@
-package vn.mn.quanlynhahang.model;
+package vn.mn.quanlynhahang.repository;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.HashMap;
+import java.util.Map;
+
+import vn.mn.quanlynhahang.model.User;
 
 public class SignUpRepository {
     private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db;
 
     public SignUpRepository() {
         firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
     public Task<AuthResult> createUserWithEmailAndPassword(String email, String password) {
         return firebaseAuth.createUserWithEmailAndPassword(email, password);
+    }
+    public Task<Void> saveUserInfoToFirestore(User user, String userId) {
+        return db.collection("users")
+                .document(userId)
+                .set(user);
     }
 }
